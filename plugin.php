f248dabb59963a9ecb7111ffa97d3d801761b9bf
kennethreitz <me@kennethreitz.com>
2009-12-16 21:09:25 -0500

    all ready to go

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- Plugin Name: Google Chrome Frame
+ Plugin Name: Trigger Google Chrome Frame
  Description: Triggers your site to be rendered with <a href="http://code.google.com/chrome/chromeframe/" target="_none">Google Chrome Frame</a> if available. And it's all thanks to <a href="http://kennethreitz.com">Kenneth Reitz</a>!
  Author: Kenneth Reitz
  Author URI: http://kennethreitz.com
@@ -9,8 +9,8 @@
 
 add_action('admin_menu', 'gopenid_admin_menu');
 
-function gopenid_snippet() {  ?>
+function chromeframe_snippet() {  ?>
 	<meta http-equiv="X-UA-Compatible" content="chrome=1"/> 
 <?php }
 
-add_action('wp_head', 'gopenid_snippet');
+add_action('wp_head', 'chromeframe_snippet');
